143c38b0dfe9c82975a8dbce638456250a499523
Sparkwz <wangzhen20221@163.com>
2025-05-31 23:26:59 +0800

    Update GEO表达数据注释.R

--- GEO表达数据注释.R
+++ GEO表达数据注释.R
@@ -15,7 +15,7 @@ gset
 a=exprs(gset[[1]])
 a[1:4,1:4]
 gset[[1]]@annotation
-#将平台号输入下方
+#将平台号输入下方（若idmap1无法获取注释数据，改用idmap2获取）
 b=idmap1::getIDs("GPL6102")
 #b=idmap2::get_soft_IDs("GPL6102")
 head(b)
